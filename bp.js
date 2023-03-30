aad213d0233ea3ea5eb161837dfd322e19e5592e
Orlundo Hubbard <weareprojectcode@gmail.com>
2023-03-29 22:52:30 -0500

    first commit

---
+++ bp.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+const API_KEY = '';
+const API_URL = ''
+
+async function generateBusinessPlan() {
+    try {
+        const prompt = 'Write a business for a new startup in the business space ';
+        const maxTokens = 1024;
+        const response = await axios.post(API_URL, )
+    }
+}
